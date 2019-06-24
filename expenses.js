7bdb48c03fa3d5fe8bcee9dae344b149f32fde64
Shaman <justoneyoureffort@gmail.com>
2019-06-25 00:25:19 +0300

    4 lesson in process

--- expenses.js
+++ expenses.js
@@ -9,37 +9,27 @@ let appData = {
     income: [],
     savings: false
 };
-let i = 2;
-while(i) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = +prompt("Во сколько обойдется?", "");
-        if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-            appData.expenses[a] = b;
-        }
-        else {
-            
-        }
-        i--;
+function detectDayBudjet() {
+    for( let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = +prompt("Во сколько обойдется?", "");
+            if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+                appData.expenses[a] = b;
+            }
+    }
+
+    appData.moneyPerDay = appData.budjet / 30;
+    alert(parseInt(appData.moneyPerDay));
 }
-// for( let i = 0; i < 2; i++) {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//         b = +prompt("Во сколько обойдется?", "");
-//         if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-//             appData.expenses[a] = b;
-//         }
-//         else {
-
-//         }
-// }
-appData.moneyPerDay = appData.budjet / 30;
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
+detectDayBudjet();
 if(appData.moneyPerDay < 100) {
     console.log("Минимальный уровень достатка");
 } else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
     console.log("Средний уровень достатка");
 } else if(appData.moneyPerDay > 2000) {
     console.log("Высокий уровень достатка");
+} else {
+    console.log("Уровень достатка не определен!")
 }
 
 
